ab9d5d3852f8d00a55c43394865acc016ca0bc13
amy.smith <amy.smith@vee24.com>
2025-01-31 13:18:14 +0000

    window.setInterval to run callback function every 1000ms

---
+++ src/controller.js
@@ -0,0 +1,16 @@
+function Controller () {
+    this.initialiseSea();
+    }
+
+    Controller.prototype.initialiseSea = function initialiseSea() {
+    window.setInterval(() => {}, 1000)
+    console.log("Hello")
+    };
+    /*initialiseSea() {
+        const background = [
+            './images/water0.png',
+            './images/water1.jpg',
+        ];
+        const backgroundIndex = 0;
+
+    }*/
