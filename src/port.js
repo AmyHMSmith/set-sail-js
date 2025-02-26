cb8f2494d0627b5b953bda46d2524da9471dd84c
amy.smith <amy.smith@vee24.com>
2025-02-26 22:31:56 +0000

    add and remove ship methods added to port class

--- src/port.js
+++ src/port.js
@@ -1,6 +1,20 @@
 class Port {
     constructor(name) {
         this.name = name;
-        this.dockedShip = [];
-    }
+        this.dockedShips = [];
+    };
+
+    addShip(ship) {
+        this.dockedShips.push(ship);
+    };
+
+    removeShip(ship) {
+        this.dockedShips = this.dockedShips.filter(ship => ship !== ship);
+    };
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = Port;
+} else {
+    window.Port = Port;
 }
