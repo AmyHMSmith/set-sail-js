dd41dfc5448831b51ff42754e86148b1be8a68ed
amy.smith <amy.smith@vee24.com>
2025-02-28 13:25:31 +0000

    encapsulated port obj so not on top level scope

--- src/port.js
+++ src/port.js
@@ -1,20 +1,25 @@
-class Port {
-    constructor(name) {
-        this.name = name;
-        this.dockedShips = [];
-    };
+// function invoked as soon as it is declared () at end of wrapper
+(function exportPort() {
+    class Port {
+        constructor(name) {
+            this.name = name;
+            this.dockedShips = [];
+        };
 
-    addShip(ship) {
-        this.dockedShips.push(ship);
-    };
+        addShip(ship) {
+            this.dockedShips.push(ship);
+        };
 
-    removeShip(ship) {
-        this.dockedShips = this.dockedShips.filter(ship => ship !== ship);
-    };
-}
-
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = Port;
-} else {
-    window.Port = Port;
-}
+        removeShip(ship) {
+            this.dockedShips = this.dockedShips.filter(ship => ship !== ship);
+        };
+    }
+    // check if in Node.js env
+    if (typeof module !== 'undefined' && module.exports) {
+        // if yes, then export Port from the module
+        module.exports = Port;
+    } else {
+        // otherwise attach the Port object to the window object in the browser environment
+        window.Port = Port;
+    }
+}());
